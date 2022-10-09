831306c3ae6e8417150e44dbdb406b55fa990661
madisonlowe <mcourteney@live.com>
2022-10-09 23:23:06 +0100

    Editing tests.

--- test/validate_test.js
+++ test/validate_test.js
@@ -6,71 +6,63 @@ const Incident = mongoose.model("Incident", IncidentSchema);
 
 describe("Validating the schema", function () {
   it("Should throw vaidation error when missing 'type' KV pair", async function () {
-    const newIncident = new Incident({
+    const incidentObject = {
       when: "Test time of day or date description.",
       description: "Test incident.",
       location: {
         address: {
           firstline: "14 Testing Drive",
-          secondline: "Test Village",
           postcode: "T35 T1NG",
-          city: "Practiceford",
-          county: "Testshire",
         },
-        what3words: "test.practice.tryout",
       },
-    });
+    };
+
+    const newIncident = new Incident(incidentObject);
   });
 
   it("Should throw vaidation error when missing 'when' KV pair", async function () {
-    const newIncident = new Incident({
+    const incidentObject = {
       type: "Validation Test",
       description: "Test incident.",
       location: {
         address: {
           firstline: "14 Testing Drive",
-          secondline: "Test Village",
           postcode: "T35 T1NG",
-          city: "Practiceford",
-          county: "Testshire",
         },
-        what3words: "test.practice.tryout",
       },
-    });
+    };
+
+    const newIncident = new Incident(incidentObject);
   });
 
   it("Should throw vaidation error when missing 'description' KV pair", async function () {
-    const newIncident = new Incident({
+    const incidentObject = {
       type: "Validation Test",
       when: "Test time of day or date description.",
       location: {
         address: {
           firstline: "14 Testing Drive",
-          secondline: "Test Village",
           postcode: "T35 T1NG",
-          city: "Practiceford",
-          county: "Testshire",
         },
-        what3words: "test.practice.tryout",
       },
-    });
+    };
+
+    const newIncident = new Incident(incidentObject);
   });
 
   it("Should not throw validation error.", async function () {
-    const newIncident = new Incident({
+    const incidentObject = {
       type: "Validation Test",
       when: "Test time of day or date description.",
       description: "Test incident.",
       location: {
         address: {
           firstline: "14 Testing Drive",
-          secondline: "Test Village",
           postcode: "T35 T1NG",
-          city: "Practiceford",
-          county: "Testshire",
         },
-        what3words: "test.practice.tryout",
       },
-    });
+    };
+
+    const newIncident = new Incident(incidentObject);
   });
 });
