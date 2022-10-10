2bf71cdf9b67c66243af1ef5839c8c9460a37bf8
madisonlowe <mcourteney@live.com>
2022-10-10 15:33:26 +0100

    Fixed update_test.js.

--- test/update_test.js
+++ test/update_test.js
@@ -1,45 +1,37 @@
-// import mongoose from "mongoose";
-// import assert from "assert";
+import mongoose from "mongoose";
+import { expect, assert, should } from "chai";
 
-// import { testIncidentSchema } from "../src/models/incidents.models.js";
-// const TestIncident = mongoose.model("TestIncident", testIncidentSchema);
+import { testIncidentSchema } from "../src/models/incidents.models.js";
+const TestIncident = mongoose.model("TestIncident", testIncidentSchema);
 
-// describe("Updating an incident", () => {
-//   let incident;
+describe("Updating an incident", () => {
+  let incident;
 
-//   beforeEach(() => {
-//     incident = new TestIncident({
-//       _id: "t35tid",
-//       type: "Update Test",
-//       when: "Description of when the event occurred.",
-//       description:
-//         "Testing updating an incidentObject with Mocha in delete_test.js.",
-//       location: {
-//         address: {
-//           firstline: "14 Testing Drive",
-//           postcode: "T35 T1NG",
-//         },
-//       },
-//     });
-//     incident.save().then(() => done());
-//   });
+  beforeEach(() => {
+    incident = new TestIncident({
+      _id: "t35tid",
+      type: "Update Test",
+      when: "Description of when the event occurred.",
+      description: "Testing updating an incidentObject with Mocha.",
+      location: {
+        address: {
+          firstline: "14 Testing Drive",
+          postcode: "T35 T1NG",
+        },
+      },
+    });
+    incident.save().then(() => done());
+  });
 
-//   function helperFunc(assertion, done) {
-//     assertion
-//       .then(() => TestIncident.find({ _id: "t35tid" }))
-//       .then((incident) => {
-//         assert(incident.when === "Updated time report description.");
-//         done();
-//       })
-//       .catch((err) => done(err));
-//   }
-
-//   it("Update an incident using id", (done) => {
-//     helperFunc(
-//       incident.updateOne({ when: "Updated time report description." }),
-//       done
-//     );
-//   });
-// });
-
-// UPDATE TESTS NOT WORKING.
+  it("Update an incident using id", (done) => {
+    let filter = { _id: incident._id };
+    let update = { when: "Updated time report description." };
+    TestIncident.findByIdAndUpdate(filter, update)
+      .then(() => TestIncident.findOne(update))
+      .then((incident) => {
+        expect(incident.when).equal("Updated time report description.");
+        done();
+      })
+      .catch((err) => done(err)); // Catch is for timeout issue calling done().
+  });
+});
